d27d243be48c9bbbfe4793744dd252d75489a09f
Zahid-isu <87206801+Zahid-isu@users.noreply.github.com>
2023-02-22 03:31:43 -0600

    first commit

---
+++ lab11/DeleteList.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "node.h"
+
+void DeleteList(node** head)
+{
+    node* temp;
+    while (*head!=NULL)
+    {
+        temp = *head;
+        *head = (*head)->next;
+        free(temp);
+    }
+}
